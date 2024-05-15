e67e07a890e6e936b0295d009de0742299867b71
Ben Saint Denis <bensaintdenis@gmail.com>
2024-05-15 03:27:33 -0400

    Added logo asset v1 (Dall-E generated). Implemented initial navbar functionality with BS3 populated by dynamic routes from vue-router. - Componentized Navbar.

---
+++ src/components/NavBar.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { computed  } from 'vue'
+import { RouterLink, useRouter, useRoute } from 'vue-router'
+
+const router = useRouter()
+const currentRoute = useRoute()
+const routes = computed(() => {
+  return router.getRoutes()
+})
+</script>
+
+<template>
+  <header>
+    <nav class="navbar navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img src="@/assets/vue3-stock-tracker-logo-v1.svg" alt="" width="30" height="24" class="d-inline-block align-text-center">
+          Vue 3 Stock Tracker Demo
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+              <li v-for="route in routes" :key="route.path" class="nav-item">
+                <router-link
+                  :class="currentRoute.name === route.name ? 'active' : ''"
+                  class="nav-link"
+                  :aria-label="route.name"
+                  aria-current="page"
+                  :to="route.path">
+                  {{route.name}}
+                </router-link>
+              </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
