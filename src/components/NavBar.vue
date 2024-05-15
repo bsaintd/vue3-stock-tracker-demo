caebd51026cd7e76fb619e9dfe6856e35255ee8c
Ben Saint Denis <bensaintdenis@gmail.com>
2024-05-15 03:59:32 -0400

    NavBar.vue refactor/cleanup.

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,6 +2,8 @@
 import { computed  } from 'vue'
 import { RouterLink, useRouter, useRoute } from 'vue-router'
 
+const appTitle = 'Vue 3 Stock Tracker Demo'
+const currentRoute = useRoute()
 const routes = computed(() => {
   return useRouter().getRoutes()
 })
@@ -11,18 +13,29 @@ const routes = computed(() => {
   <header>
     <nav class="navbar navbar-light bg-light">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-          <img src="@/assets/vue3-stock-tracker-logo-v1.svg" alt="Vue 3 Stock Tracker Demo Logo" width="60" height="60" class="d-inline-block align-text-center">
-          Vue 3 Stock Tracker Demo
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <router-link class="navbar-brand" to="/">
+          <img
+            src="@/assets/vue3-stock-tracker-logo-v1.svg"
+            alt="Vue 3 Stock Tracker Demo Logo"
+            width="60"
+            height="60"
+            class="d-inline-block align-text-center">
+          {{ appTitle }}
+        </router-link>
+        <button class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav">
               <li v-for="route in routes" :key="route.path" class="nav-item">
                 <router-link
-                  :class="useRoute().name === route.name ? 'active' : ''"
+                  :class="currentRoute.name === route.name ? 'active' : ''"
                   class="nav-link"
                   :aria-label="route.name"
                   :to="route.path">
